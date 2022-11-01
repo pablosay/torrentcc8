b32e77ff693a61baa314346be8a62a8f6bd93373
pablosay <19001434@galileo.edu>
2022-10-31 18:14:25 -0600

    Log cambiado

--- Log.java
+++ Log.java
@@ -4,47 +4,31 @@ import java.io.File;
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
- 
+
 public class Log {
- 
+
     private BufferedWriter buffered;
     private String ruta;
- 
+
     public Log(String ruta) {
         try {
             this.ruta = ruta;
             File archivo = new File(ruta);
-            if(archivo.exists()){
+            if (archivo.exists()) {
                 archivo.delete();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
- 
-    private void open() {
-        try {
-            this.buffered = new BufferedWriter(new FileWriter(this.ruta, true));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-    private void close() {
-        try {
-            this.buffered.close();
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-    }
- 
     public void print(String data) {
         try {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss");
+            SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
             String formatoFecha = sdf.format(new Date());
-            this.open();
-            this.buffered.write(formatoFecha + " > " + data + "\n");
-            this.close();
+            this.buffered = new BufferedWriter(new FileWriter(this.ruta, true));
+            this.buffered.write("[" + formatoFecha + "]" + data + "\n");
+            this.buffered.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
